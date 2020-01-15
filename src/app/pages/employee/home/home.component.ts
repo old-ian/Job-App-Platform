30c3be39b6312b7d1ecd7a01f1b8e31073750046
Mark <>
2020-01-15 23:19:03 +0800

    Completed HomePage

--- src/app/pages/employee/home/home.component.ts
+++ src/app/pages/employee/home/home.component.ts
@@ -16,6 +16,116 @@ appliedJobs = [
   		company: 'Outlast Developers',
   		position: 'Senior Software Engineer',
   		location: 'Brazil'}
+  	]	
+jobLists = [ 
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000' ,
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
   	]	  	  	
   constructor() { 
   }
