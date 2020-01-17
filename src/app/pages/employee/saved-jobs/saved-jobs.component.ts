566e9d6a297f54233f28152a14f42041194daed8
Mark <>
2020-01-17 19:29:03 +0800

    added new pages

--- src/app/pages/employee/saved-jobs/saved-jobs.component.ts
+++ src/app/pages/employee/saved-jobs/saved-jobs.component.ts
@@ -7,7 +7,127 @@ import { JobService } from '../../../services/job/job.service';
   styleUrls: ['./saved-jobs.component.scss']
 })
 export class SavedJobsComponent implements OnInit {
-
+user = 'John Ezekiel';
+appliedJobs = [ 
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Software Engineer',
+  		location: 'Brazil' },
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil'}
+  	]	
+jobLists = [ 
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000' ,
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Outlast Developers',
+  		position: 'Senior Software Engineer',
+  		location: 'Brazil',
+  		offer: '13,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  		{ 
+  		company: 'Accenture',
+  		position: 'Junior Software Engineer',
+  		location: 'Metro Manila',
+  		offer: '20,000',
+  		time: '4 days ago'},
+  	]	
   constructor(private jobservice: JobService) { }
 
   ngOnInit() {
