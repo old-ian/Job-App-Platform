f53668eb98b54d60eb168f758e2cdf99d6b90c42
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-03-11 16:38:39 +0800

    Added placeholder functionality to sign up component

--- src/app/pages/company/company-signup/company-signup.component.ts
+++ src/app/pages/company/company-signup/company-signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company-signup',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanySignupComponent implements OnInit {
 
-  constructor() { }
+  nextStep = false;
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  first_name = new FormControl('', [Validators.required]);
+  last_name = new FormControl('', [Validators.required]);
+  showErrorMessage: boolean;
+  constructor(public router: Router ) { }
 
   ngOnInit() {
+
   }
 
+  getErrorMessage() {
+    const password: string = this.password.value;
+    if (this.email.hasError('required')) {
+      return 'You must enter an email';
+    }
+
+    if (this.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+    if (password.length < 6) {
+      return 'Minimum password length is 6';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getContinueResponse() {
+    if (this.first_name.hasError('required')) {
+      return 'Please Enter your First Name';
+    }
+
+    if (this.last_name.hasError('required')) {
+      return 'Please Enter your Last Name';
+    }
+
+    return '';
+  }
+
+  signUp() {
+    this.router.navigate(['/signup/set-up']);
+    if (this.getContinueResponse() !== '') {
+      this.showErrorMessage = true;
+    }
+
+  }
 }
