35ad034794cb31b050bc3cce630b5af09378e3c6
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-03-11 16:25:31 +0800

    Added company-login placeholder functionality

--- src/app/pages/company/company-login/company-login.component.ts
+++ src/app/pages/company/company-login/company-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-company-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyLoginComponent implements OnInit {
 
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter an email';
+    }
+
+    if (this.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
 }
